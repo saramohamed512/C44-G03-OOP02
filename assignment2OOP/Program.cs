379837f1a58610b2e3c78cb8301aee782934eaed
SaraMohamed-20190234 <saramohamed12656@gmail.com>
2025-07-19 17:04:23 +0300

    problem2

--- assignment2OOP/Program.cs
+++ assignment2OOP/Program.cs
@@ -6,18 +6,39 @@
         {
             #region Problem 1: Person Struct
 
-            Person[] people = new Person[3];
-            people[0] = new Person { Name = "Sara", Age = 24 };
-            people[1] = new Person { Name = "Mohamed", Age = 24 };
-            people[2] = new Person { Name = "Ali", Age = 22 };
+            //Person[] people = new Person[3];
+            //people[0] = new Person { Name = "Sara", Age = 24 };
+            //people[1] = new Person { Name = "Mohamed", Age = 24 };
+            //people[2] = new Person { Name = "Ali", Age = 22 };
 
-            // Display details of all persons
-            Console.WriteLine("Person Details:");
-            foreach (var person in people)
-            {
-                Console.WriteLine($"Name: {person.Name}, Age: {person.Age}");
-            }
+            //// Display details of all persons
+            //Console.WriteLine("Person Details:");
+            //foreach (var person in people)
+            //{
+            //    Console.WriteLine($"Name: {person.Name}, Age: {person.Age}");
+            //}
             #endregion
-        }
+            #region Problem 2: Point Struct
+            //Console.WriteLine("Enter first point coordinates:");
+            //Console.Write("X: ");
+            //double x1 = double.Parse(Console.ReadLine());
+            //Console.Write("Y: ");
+            //double y1 = double.Parse(Console.ReadLine());
+
+            //Console.WriteLine("Enter second point coordinates:");
+            //Console.Write("X: ");
+            //double x2 = double.Parse(Console.ReadLine());
+            //Console.Write("Y: ");
+            //double y2 = double.Parse(Console.ReadLine());
+
+            //Point point1 = new Point { X = x1, Y = y1 };
+            //Point point2 = new Point { X = x2, Y = y2 };
+
+            //double distance = Math.Sqrt(Math.Pow(point2.X - point1.X, 2) + Math.Pow(point2.Y - point1.Y, 2));
+            //Console.WriteLine($"Distance between the points: {distance:F2}");
+        
+            #endregion
+
+    }
     }
 }
