dd4e720775aa630675ea86c37ffb79e53668f547
SaraMohamed-20190234 <saramohamed12656@gmail.com>
2025-07-19 15:54:24 +0300

    Encapsulation [Properties]

--- ass2OOP/Program.cs
+++ ass2OOP/Program.cs
@@ -5,10 +5,24 @@
         static void Main(string[] args)
         {
             #region user defined datatype [Struct]
-            Point p1;
-            p1=new Point(10, 20);
-            //new => just for constructor selection
-            Console.WriteLine(p1);
+            //Point p1;
+            //p1=new Point(10, 20);
+            ////new => just for constructor selection
+            //Console.WriteLine(p1);
+            #endregion
+            #region OOP
+            #region encapsulation
+            Employee emp01 = new Employee(1, "John Doe", 50000);
+            //Console.WriteLine(emp01);
+            //emp01.Name="Johnathan Doe";
+            //Console.WriteLine(emp01.GetName());
+            //emp01.SetName("Johnathan Doe");
+            //Console.WriteLine(emp01.GetName());
+
+            //emp01.Salaryproperty = 8000;
+            //Console.WriteLine(emp01.Salaryproperty);
+
+            #endregion
             #endregion
         }
     }
